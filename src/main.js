8610286994483fa743b5ca3f28cdc93c5f9d7375
sabilimuhammad <m.afriansabili@gmail.com>
2021-10-06 13:34:04 +0700

    create table and get data from api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import plugins from './plugins';
 
-Vue.config.productionTip = false
+import './assets/scss/style.scss';
+
+Vue.config.productionTip = false;
+Vue.use(plugins);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Inventory/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Inventory from '@/components/Inventory.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Inventory,
   }
 }
 </script>
